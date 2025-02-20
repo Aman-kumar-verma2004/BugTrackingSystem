196d8f6e5d24e0f25da9461c9cb3349d8fcf92f3
amrit <amritkumarverma848@gmail.com>
2025-02-20 19:27:09 +0530

    First Commit

---
+++ Backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+
+const app = express();
+dotenv.config();
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+
+//Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/bug", bugRoutes);
+
+
+//mongodb connected
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDb Connected"))
+    .catch(err => {
+        console.log(err);
+    })
+
+
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server is live at ${PORT}`);
+})
+
