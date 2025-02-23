023a26bfc903e1e3c2178b12aeca531267ade12b
amrit <amritkumarverma848@gmail.com>
2025-02-23 16:06:10 +0530

    Tailwinf added

---
+++ Frontend/Bug-tracker/src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div className='flex justify-center align-center'>
+        Hii guyz
+    </div>
+  )
+}
+
+export default Register
